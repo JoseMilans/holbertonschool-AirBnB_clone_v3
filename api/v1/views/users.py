edda5468ec32fa776d26184a53745d3e15a16c4a
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2023-12-24 16:33:05 -0300

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,21 +8,23 @@ from models import storage, User
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
-    """Retrieves the list of all User objects"""
+    """Retorna todos los usuarios"""
     users = [user.to_dict() for user in storage.all(User).values()]
     return jsonify(users)
 
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
-    """Retrieves a User object"""
+    """Returns 1 user"""
     user = storage.get(User, user_id)
     if user:
         return jsonify(user.to_dict())
     abort(404)
 
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
-    """Deletes a User object"""
+    """Deletes User """
     user = storage.get(User, user_id)
     if user:
         storage.delete(user)
@@ -30,9 +32,10 @@ def delete_user(user_id):
         return jsonify({}), 200
     abort(404)
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
-    """Creates a User"""
+    """ Creates a User For the Airbnb Clone"""
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
@@ -45,9 +48,10 @@ def create_user():
     new_user.save()
     return jsonify(new_user.to_dict()), 201
 
+
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
-    """Updates a User object"""
+    """Updates a User object , change value"""
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -55,10 +59,12 @@ def update_user(user_id):
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
-    
+
+
     ignore_keys = ['id', 'email', 'created_at', 'updated_at']
     for key, value in data.items():
         if key not in ignore_keys:
             setattr(user, key, value)
     user.save()
     return jsonify(user.to_dict()), 200
+    
